22659f362e613487e73bf450e3e8b6998fbffaf1
Dimitris <dimitrisbaziot@yahoo.com>
2025-05-25 23:17:06 +0300

    finished project

--- travel_time.py
+++ travel_time.py
@@ -6,17 +6,14 @@ def connect_sequential_stations(graph, df):
         curr = df_sorted.iloc[i]
         next_ = df_sorted.iloc[i + 1]
 
-        # If on the same line and sequential in number
         if curr['line'] == next_['line'] and next_['num'] - curr['num'] == 1:
             weight = random.randint(2, 8)
             graph.connect(curr['code'], next_['code'], weight=weight)
 
 def connect_transfer_stations(graph, df, transfer_weight=5):
-    # Group by station name (interchanges)
     grouped = df.groupby('name')
     for name, group in grouped:
         codes = group['code'].tolist()
-        # Only connect if there's more than 1 code (i.e., a transfer)
         if len(codes) > 1:
             for i in range(len(codes)):
                 for j in range(i + 1, len(codes)):
